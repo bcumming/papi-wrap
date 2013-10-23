9221a50f70e786b4993eb1b78ca4f04d8f027445
bcumming <bcumming@santis01.(none)>
2013-10-23 17:20:55 +0200

    add openmp to dot test case

--- test/dot.c
+++ test/dot.c
@@ -7,32 +7,36 @@ int main(void){
     int handle1=-1;
     int handle2=-1;
     int i;
-    int N=10000000;
+    int N=1000000;
     double *a = (double*)malloc(N*sizeof(double));
     double *b = (double*)malloc(N*sizeof(double));
     double sum=0.;
 
-    handle1 =  pw_new_collector("counter 1");
-    handle2 =  pw_new_collector("counter 2");
+    handle1 =  pw_new_collector("initialize");
+    handle2 =  pw_new_collector("dot product");
 
     printf( "two collectors with handles %d and %d\n", handle1, handle2);
 
     // the first counter counts the initialization loop
     pw_start_collector(handle1);
+    #pragma omp parallel for
     for(i=0; i<N; i++) {
-        a[i] = (double)i;
-
-        b[i] = 0.1*(double)i;
+        a[i] = 0.001*(double)i;
+        b[i] = 0.001*(double)i;
     }
     pw_stop_collector(handle1);
 
     // the second counter counts the dot product loop
     pw_start_collector(handle2);
     sum = 0.;
+    #pragma omp parallel for reduction(+:sum)
     for(i=0; i<N; i++) {
         sum += a[i]*b[i];
     }
     pw_stop_collector(handle2);
+
+    // print the result so that the compiler doesn't remove it when optimizing
+    printf("the sum is %g\n", sum);
 
     pw_print();
 }
