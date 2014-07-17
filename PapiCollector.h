786e5916ab4d6643b141e13df8d32641fd9635e4
mbianco <mbianco@cscs.ch>
2014-07-17 12:25:33 +0200

    ifdefs to exclude papi. need an extra member if not using papi.

--- PapiCollector.h
+++ PapiCollector.h
@@ -8,7 +8,9 @@
 #include <vector>
 #include <string>
 
+#ifndef NOPAPI
 #include <papi.h>
+#endif
 #include <omp.h>
 
 class PapiCollector {
@@ -36,5 +38,8 @@ private:
     std::vector<int> numbers_;
     std::vector<double> times_;
     std::vector<std::vector<long long> > counts_;
+#ifdef NOPAPI
+    std::vector<double> timeVec_;
+#endif
 };
 
