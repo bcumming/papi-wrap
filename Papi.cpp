618cbe165ce7d0768f179aadf3fbcff0a64fb777
mbianco <mbianco@cscs.ch>
2014-07-17 12:23:33 +0200

    removed ostreams from here

--- Papi.cpp
+++ Papi.cpp
@@ -36,24 +36,6 @@ void Papi::init() {
 
     int papi_error;
 
-    #ifdef PW_MPI
-    std::stringstream debug_fname;
-    debug_fname << "papi_debug";
-    int mpi_rank = -1;
-    int mpi_size = -1;
-
-    // assume that MPI has already been initialized...
-    MPI_Comm_size(MPI_COMM_WORLD,&mpi_size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&mpi_rank);
-
-    // write to file
-    debug_fname << "_" << mpi_rank << "_" << mpi_size;
-    debug_fname << ".txt";
-    fid_.open(debug_fname.str().c_str());
-    #else
-    std::ostream &fid_ = std::cerr;
-    #endif
-
     // set debugging if requested by environment variable
     char *debugStr = getenv("CSCSPERF_DEBUG");
     debug_ = (debugStr != NULL);
